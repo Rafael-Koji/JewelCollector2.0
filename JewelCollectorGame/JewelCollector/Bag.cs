368cdcfdfcacf9b9dcc2d22d98a556649388f180
cristiano-campos <cristianocampos1996@hotmail.com>
2023-06-11 22:13:11 -0300

    Finish the XML Documentation

--- JewelCollectorGame/JewelCollector/Bag.cs
+++ JewelCollectorGame/JewelCollector/Bag.cs
@@ -1,13 +1,35 @@
 namespace JewelCollectorGame;
 
+/// <summary>
+/// Represents a Bag in the JewelCollector game. This is used to store collected Jewels.
+/// </summary>
 public class Bag
 {
+    /// <summary>
+    /// Represents the score based on the jewels collected.
+    /// </summary>
     private int score;
+
+    /// <summary>
+    /// Represents the number of jewels in the bag.
+    /// </summary>
     private int numJewels;
+
+    /// <summary>
+    /// List of jewels in the bag.
+    /// </summary>
     List<Jewel> jewels = new List<Jewel>();
+
+    /// <summary>
+    /// Inserts a jewel in the bag.
+    /// </summary>
     public void InsertInBag(Jewel jewel){
         jewels.Add(jewel);
     }
+
+     /// <summary>
+    /// Counts the score based on the jewels in the bag.
+    /// </summary>
     private void CountScore(){
         score = 0;
         foreach(Jewel j in jewels){
@@ -24,13 +46,25 @@ public class Bag
             }            
         }
     }
+
+    /// <summary>
+    /// Returns the total score based on the jewels in the bag.
+    /// </summary>
     public int GetScore(){
         CountScore();
         return score;
     }
+
+    /// <summary>
+    /// Counts the total number of jewels in the bag.
+    /// </summary>
     private void CountJewels(){
         numJewels = jewels.Count();
     }
+
+    /// <summary>
+    /// Returns the total number of jewels in the bag.
+    /// </summary>
     public int GetTotalJewels(){
         CountJewels();
         return numJewels;
--- JewelCollectorGame/JewelCollector/ICell.cs
+++ JewelCollectorGame/JewelCollector/ICell.cs
@@ -1,10 +1,27 @@
 namespace JewelCollectorGame;
 
+/// <summary>
+/// Represents a Cell in the JewelCollector game.
+/// </summary>
 public interface ICell
-{
+{   /// <summary>
+    /// Represents the x-coordinate of the Cell on the map.
+    /// </summary>
     public int x{get;set;}
+
+    /// <summary>
+    /// Represents the y-coordinate of the Cell on the map.
+    /// </summary>
     public int y{get;set;}
+
+    /// <summary>
+    /// Represents the symbol that will represent the Cell on the map.
+    /// </summary>
     public string objRepresentation{get;set;}
+
+     /// <summary>
+    /// Represents whether the Cell's current position blocks movement.
+    /// </summary>
     public bool blockMovement{get;set;}
 
 }
